19809ea9361812f88ddb9b8596e11408e3495b1a
Zeran Lu <zeranlu0326@gmail.com>
2025-04-16 10:58:14 -0400

    updated javascript
    In this commit, the broken graphics of the javascript pop up promo boxes were fixed.
    
    additionally, another data array called "flavourprofile" was added for each promo box.

--- js/main.js
+++ js/main.js
@@ -7,26 +7,30 @@ const promos = [
     {
         id: "berry-blast",
         name: "Product Launch Special Promotion!",
-        description: "Try the brand new Orbitz! Buy one, get one free! Limited time offer ONLY.1",
-        image: "images/promo_berry.png"
+        description: "Try the brand new Orbitz! Buy one, get one free! Limited time offer ONLY.",
+        flavourprofile: "Bursting with the juicy sweetness of ripe berries, this soda packs a fruity punch that’s both refreshing and bold. Each sip delivers a vibrant blend of berry goodness with a fizzy twist that keeps you coming back for more.",
+        image: "images/def-mob.png"
     },
     {
         id: "root-beer",
         name: "Root Beer Promotion",
         description: "Duo featured new flavours for our launch!",
-        image: "images/promo_rootbeer.png"
+        flavourprofile: "Rich, smooth, and delightfully nostalgic, this cream soda delivers a velvety vanilla flavour with every fizzy sip. It’s a sweet throwback that’s as comforting as it is refreshing.",
+        image: "images/root-beer-mob.png"
     },
     {
         id: "cream-soda",
         name: "Creamy Dream Promotion",
         description: "Taste the rich and sweet profiles of both!",
-        image: "images/promo_cream.png"
+        flavourprofile: "This root beer brings a bold, earthy kick with hints of warm spice and creamy sweetness. It's a timeless flavour that fizzes with nostalgia and leaves a satisfying finish.",        
+        image: "images/cream-mob.png"
     },
     {
         id: "spiderman",
         name: "Limited Edition",
         description: "Spiderman-themed Orbitz. Collect them all!",
-        image: "images/promo_spiderman.png"
+        flavourprofile: "Swing into action with our limited-edition Spiderman soda — a heroic blend of bold flavour and unstoppable fizz. Packed with punch and pop, this collectible drink is made for thrill-seekers and true fans alike.",
+        image: "images/spiderman-mob.png"
     }
 ];
 const productCards = document.querySelectorAll(".product-card");
@@ -43,8 +47,10 @@ function showPromo(promoObj) {
     promoBox.innerHTML = 
         `
         <div class="promo-content">
+            <h3>${promoObj.name}</h3>
             <img src="${promoObj.image}">
-            <p>${promoObj.name}</p>
+            <p>${promoObj.description}</p>
+            <p>${promoObj.flavourprofile}</p>
             <button class="close-btn">X</button>
         </div>
         `;
